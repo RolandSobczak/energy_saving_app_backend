05399b974830fe4c270993ee0f535f5fe4a06a77
Roland Sobczak <rolandsobczak@gmail.com>
2022-09-24 12:53:52 +0200

    cors policy update

--- energy/settings.py
+++ energy/settings.py
@@ -114,6 +114,15 @@ LOGOUT_REDIRECT_URL = "/account/login"
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
